55b04ce33755df7effba1494bcc25f58cd3d05eb
Maradana hemalatha <hemamaradana5@gmail.com>
2026-04-28 18:12:58 +0530

    frontend ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,15 @@
 export const environment = {
+  // production: true,
+  // authUrl: 'https://ai-saas-backend-drf7.onrender.com',
+  // resumeUrl: 'https://ai-resume-bot-production-d7b2.up.railway.app',
+  // chatUrl: 'https://ai-chatbot-backend-whdf.onrender.com',
+  // agentUrl: ''
+
+
   production: true,
   authUrl: 'https://ai-saas-backend-drf7.onrender.com',
   resumeUrl: 'https://ai-resume-bot-production-d7b2.up.railway.app',
   chatUrl: 'https://ai-chatbot-backend-whdf.onrender.com',
-  agentUrl: ''
+  agentUrl: null
 };
-//
+
