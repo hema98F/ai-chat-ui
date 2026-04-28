43d942a3e2d683064bf2f62e3cb9a8f3914138a1
Maradana hemalatha <hemamaradana5@gmail.com>
2026-04-28 17:19:01 +0530

    fix: update prod environment with real URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  authUrl: 'https://your-auth-railway-url.up.railway.app',
+  authUrl: 'https://ai-saas-backend-drf7.onrender.com',
   resumeUrl: 'https://ai-resume-bot-production-d7b2.up.railway.app',
-  agentUrl: 'https://your-agent-railway-url.up.railway.app',
   chatUrl: 'https://ai-chatbot-backend-whdf.onrender.com',
+  agentUrl: ''
 };
