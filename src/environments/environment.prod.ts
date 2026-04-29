f31a356821232a04ab529a7476d008eb2ed236e6
Maradana hemalatha <hemamaradana5@gmail.com>
2026-04-29 11:43:19 +0530

    changed env prod link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,7 @@
 export const environment = {
-  // production: true,
-  // authUrl: 'https://ai-saas-backend-drf7.onrender.com',
-  // resumeUrl: 'https://ai-resume-bot-production-d7b2.up.railway.app',
-  // chatUrl: 'https://ai-chatbot-backend-whdf.onrender.com',
-  // agentUrl: ''
-
-
   production: true,
   authUrl: 'https://ai-saas-backend-drf7.onrender.com',
   resumeUrl: 'https://ai-resume-bot-production-d7b2.up.railway.app',
   chatUrl: 'https://ai-chatbot-backend-whdf.onrender.com',
-  agentUrl: null
+  agentUrl: 'https://ai-agent-g0fl.onrender.com',
 };
-
